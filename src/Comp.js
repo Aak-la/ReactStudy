12a03855c19cc584894211b9e8e5e742302d7bda
迷途之枪 <y486910173@163.com>
2021-12-05 17:59:00 +0800

    结束

---
+++ src/Comp.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+export default class Comp extends Component {
+  state = {
+    n: 0,
+  };
+  handleClick = () => {
+     this.setState({
+      n: this.state.n + 1,
+    },()=>{
+      console.log(this.state.n);
+    });
+    this.setState((target) => {
+     
+      return {
+        n: target.n + 2,
+      };
+      
+    });
+  };
+  render() {
+    return (
+      <div>
+        <h1>{this.state.n}</h1>
+        <p>
+          <button onClick={this.handleClick}>+</button>
+        </p>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Pager from './components/PagerTest'
+import Comp from './Comp'
 
-
-ReactDOM.render(<Pager />, document.getElementById('root'));
+ReactDOM.render(<Comp />, document.getElementById('root'));
+ 
